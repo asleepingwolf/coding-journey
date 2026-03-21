254cd5dd9f5eeb5f2b688fbc94a03913f854510e
asleepingwolf <soerensenjonathan@gmail.com>
2026-03-21 20:12:00 +0100

    Changed to-do list and keep on learning git

--- To-dolistan.py
+++ To-dolistan.py
@@ -1,16 +1,21 @@
 klar = False
-list = []
-while klar == False:
-    choice = input('Vad vill du göra? ' \
-    '1. Lägg till på Att göra listan. ' \
-    '2. Ta bort från listan. ' \
-    '3. Visa hela listan. ' \
-    '4. Avsluta ')
+todo_list = [] # Ändrade namnet från 'list'
+
+while not klar: # Snyggare sätt att skriva 'klar == False'
+    # Lade till \n för att göra menyn mycket tydligare i terminalen
+    choice = input('Vad vill du göra?\n' \
+                   '1. Lägg till på Att göra listan.\n' \
+                   '2. Ta bort från listan.\n' \
+                   '3. Visa hela listan.\n' \
+                   '4. Avsluta\n' \
+                   'Ditt val: ')
+                   
     if int(choice) == 1:
-        list.append(input('Vad vill du lägga till?'))
+        todo_list.append(input('Vad vill du lägga till? '))
     elif int(choice) == 2:
-        list.remove(input('Vad vill du ta bort?'))
+        todo_list.remove(input('Vad vill du ta bort? '))
     elif int(choice) == 3:
-        print(list)
+        print(f"Här är din lista: {todo_list}")
     elif int(choice) == 4:
         klar = True
+        print("Hej då!")
