bdd473cbd89d654b8fdd11fea0f993203bd9458b
asleepingwolf <soerensenjonathan@gmail.com>
2026-03-21 20:42:54 +0100

    Touched up the code a bit, removed negative damage and error fix with continue.

--- dmgcalculator.py
+++ dmgcalculator.py
@@ -1,14 +1,18 @@
 import random
 def calculate_damage(attack, armor):
-    crit = random.randint(0, 1)
+    dmg = attack - armor
+    if dmg <= 0:
+        return (0)
+    crit = random.randint(0, 3)
     if crit == 1:
-        print('Critical hit! Damage done:', (attack - armor) * 2)
+        print('Critical hit!')
+        return(dmg * 2)
     else:
-        print('Damage done:', attack - armor)
+        return(dmg)
 
 running = True
 while running == True:
-    print('Welcome to the damage calculator! För att avsluta skriv "stop"')
+    print('Welcome to the damage calculator! To stop, write: "stop"')
     temp = input('Attack: ')
     if temp == 'stop':
         break
@@ -16,6 +20,7 @@ while running == True:
         atk = int(temp)
     except:
         print('Error: Bara siffror')
+        continue
     temp = input('Armor: ')
     if temp == 'stop':
         break
@@ -23,4 +28,5 @@ while running == True:
         armor = int(temp)
     except:
         print('Error: Bara siffror')
-    calculate_damage(atk, armor)
+        continue
+    print('You did:', calculate_damage(atk, armor),'damage!')
