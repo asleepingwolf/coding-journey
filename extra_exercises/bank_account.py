58c84cdcfc2e709da9fb656c5c57b9963cfa376c
asleepingwolf <soerensenjonathan@gmail.com>
2026-03-26 20:14:56 +0100

    Updated banking app

--- extra_exercises/bank_account.py
+++ extra_exercises/bank_account.py
@@ -24,26 +24,35 @@ class Person(Bank_account):
 
 kund1 = Person('John', 'Doe', 1203, '123abc')
 kund2 = Person('Julia', 'Krigare', 63589, '123456')
+banking = None
 
 customers = [kund1, kund2]
-
+loggedin = False
 print(f'Welcome to Python Bank! Please log in. \n')
 while loggedin == False:
-    startup_choice = input(f'What do you want to do? 1. Log in. \n 2. Check accounts \n 3. Open new account \n')
-    if int(startup_choice) == 1:
-        input_name = input('Account name:')
-        input_pass = input(f'Password: ')
-        #Trying to login
-        for c in customers:
-            if c.login(input_name, input_pass) == True:
-                banking = c
-                loggedin = True
-                break
-    elif int(startup_choice) == 2:
-        for c in customers:
-            print(f'{c.first_name} \n')
-    elif int(startup_choice) == 3:
-        kund3 = Person(input(f'Name: '), f'\n Last name: ', 0, f'\n Password: ')
+    startup_choice = input(f'What do you want to do? \n 1. Log in. \n 2. Check accounts \n 3. Open new account \n')
+    try:
+        if int(startup_choice) == 1:
+            input_name = input('Account name:')
+            input_pass = input(f'Password: ')
+            #Trying to login
+            for c in customers:
+                if c.login(input_name, input_pass) == True:
+                    banking = c
+                    loggedin = True
+                    break
+        elif int(startup_choice) == 2:
+            for c in customers:
+                print(f'{c.first_name} \n')
+        elif int(startup_choice) == 3:
+            new_account_name = input('Name: ')
+            new_account_lname = input('Last name: ')
+            new_account_pass = input('Password: ')
+            kund3 = Person(new_account_name, new_account_lname,0 , new_account_pass)
+            customers.append(kund3)
+    except ValueError:
+        print('Value error')
+        continue
 
 
 
