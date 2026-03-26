1bff41b76b687a92d58268e08f3464a11c9a7f6c
asleepingwolf <soerensenjonathan@gmail.com>
2026-03-26 22:43:16 +0100

    Started Work on text-adventure

--- text-adventure.py
+++ text-adventure.py
@@ -1,17 +1,55 @@
 import random
-def health_loss(currenthealth, damage):
-    remaining_health = currenthealth - damage
 
-def player():
-    def playerhealth():
-        
-def choices():
-    random(0,3)
-    input()
+#Main class for all characters 
+class Character():
+    def __init__(self, name, health, damage):
+        self.name = name
+        self.health = health
+        self.damage = damage
+
+#Player class, weapons inventory
+class PlayerClass(Character):
+    def __init__(self, name, health, damage):
+        super().__init__(name, health, damage)
+        self.weapon = 'Fists'
+        self.inventory = {}
+        def InventoryAdd(self, item):
+            self.inventory.append(item)
+
+#Monster Class
+class MonsterClass(Character):
+    def __init__(self, name, health, damage):
+        super().__init__(name, health, damage)
+
+class RoomClass():
+    def __init__(self, luck):
+        self.luck = luck
+    def loot(self):
+        if self.luck >= 4:
+            #temp = random.randint(0,3)
+            temp = 0
+            if temp == 0:
+                player.inventory('Health Potion')
+
+#Initialising the player
+player = PlayerClass('Julia', 100 , 5)
+
+print(player.name)
+print(player.weapon)
+print(f' Du har: {player.inventory}')
 
 
-print('Welcome To my First Text Adventure!')
-print('You wake up in a dark room, all you can see is the sword in your hand and the door in front of you!')
-slut = False
-while not slut:
-    choices()
+room1 = RoomClass(5)
+room1.loot()
+
+print(f'Welcome {player.name}! \n ')
+print(player.inventory)
+choice = input(f'You stand in front of a door to a giant fortress. What would you like to do? \n 1. Enter \n 2 Leave \n ')
+if int(choice) == 1:
+    playing = True
+else:
+    print(f'You go home and live your boring normal life. \n ---GAME OVER!---')
+    exit()
+#Game loop
+while playing == True:
+    exit()
